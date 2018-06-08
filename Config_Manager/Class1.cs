6d8360f04fc037b81d399488bd4e626d456a1781
Major Sándor <major.sanyi@outlook.com>
2018-06-08 12:26:14 +0200

    Upgrading Create wizard with comments

--- Config_Manager/Class1.cs
+++ Config_Manager/Class1.cs
@@ -12,13 +12,18 @@ namespace Config_Helper {
         public void CreateConfig() {
             Dictionary<string, string> settings = new Dictionary<string, string>();
             string input;
+            int comment = 0;
             while (true) {
-                Console.WriteLine("Írd be a következő beállítás nevét(Üres ha itt a vége)");
+                Console.WriteLine("Írd be a következő beállítás nevét(Üres ha itt a vége,# ha komment)");
                 input = Console.ReadLine().Replace(" ", String.Empty);
                 if (input == "0" || input == "")
                     break;
                 else if (settings.ContainsKey(input))
                     Console.WriteLine("Ilyen nevű beállítás már van!!");
+                else if (input.StartsWith("#")) {
+                    Console.WriteLine("Kérem a kommentet");
+                    settings.Add($"#komment{comment++}", Console.ReadLine().Replace(" ", String.Empty));
+                }
                 else {
                     Console.WriteLine("Kérem az értékét!");
                     settings.Add(input, Console.ReadLine().Replace(" ", String.Empty));
@@ -31,6 +36,9 @@ namespace Config_Helper {
             Console.WriteLine("Hova mentsem el?:");
             using (var sw = new StreamWriter(Console.ReadLine().Replace(" ", String.Empty)))
                 foreach (var item in settings) {
+                    if (item.Key.StartsWith("#")) {
+                        sw.WriteLine($"#{item.Value}");
+                    }
                     sw.WriteLine($"{item.Key}: {item.Value}");
                 }
         }
